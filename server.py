7d4bbe3457eb431d3e52ab17047044fedcf1f3af
dianka759 <dianka759@gmail.com>
2021-10-30 00:19:25 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe, hush hush!'
+
+@app.route('/')
+def counter():
+    if 'counter' in session:
+        session['counter'] = session.get('counter') + 1
+    else:
+        session['counter'] = 1
+    return render_template("index.html")
+
+@app.route('/destroy')
+def destroy():
+    session.clear()
+    return redirect("/")
+
+@app.route('/reset')
+def reset():
+    if 'counter' in session:
+        session['counter'] = 0
+    return redirect("/")
+
+@app.route('/plusTwo')
+def plusTwo():
+    if 'counter' in session:
+        session['counter'] = session.get('counter') + 1
+    return redirect("/")
+
+@app.route('/yourNumber', methods=['POST'])
+def yourNumber():
+    session['counter'] = session.get('counter') + int(request.form['yourNumber']) - 1
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
